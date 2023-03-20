a487210edad52491c531c62fd995a9377dfe6e0c
tgthiag <tgthiag@gmail.com>
2023-03-20 03:13:12 -0300

    Printing OK, Ready to deploy V1.0

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,11 +1,12 @@
 import 'dart:io';
 import 'dart:ui';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:syncfusion_flutter_barcodes/barcodes.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:printing/printing.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -19,7 +20,6 @@ class _MyHomePageState extends State<MyHomePage> {
   final TextEditingController txtController = TextEditingController();
   final GlobalKey globalKey = GlobalKey();
 
-
   void _printLatestValue() {
     setState(() {
       txtController.text = txtController.text.toString();
@@ -30,15 +30,10 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-
           children: <Widget>[
             const Text(
               'Digite a rua e a posição:',
@@ -52,105 +47,92 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               onSubmitted: (value) => _printLatestValue(),
             ),
-
             Container(
-              width: 300,
-              height: 500,
-              margin: EdgeInsets.all(10),
-              child:  RepaintBoundary(
-                key: globalKey,
-                child:
-                Column(
-                  children: <Widget>[
+                width: 200,
+                height: 500,
+                margin: EdgeInsets.all(10),
+                child: RepaintBoundary(
+                  key: globalKey,
+                  child: Column(children: <Widget>[
                     Expanded(
-                      flex: 3, // takes 30% of available width
-                      child:       SfBarcodeGenerator(
+                      flex: 8, // takes 30% of available width
+                      child: SfBarcodeGenerator(
                         value: "${txtController.text}.1",
                         showValue: true,
+                        textStyle: const TextStyle(fontWeight: FontWeight.bold),
                       ),
                     ),
-                    const Spacer(),
+                    const SizedBox(height: 20),
                     Expanded(
-                      flex: 3, // takes 30% of available width
-                      child:       SfBarcodeGenerator(
+                      flex: 8, // takes 30% of available width
+                      child: SfBarcodeGenerator(
                         value: "${txtController.text}.2",
                         showValue: true,
+                        textStyle: const TextStyle(fontWeight: FontWeight.bold),
                       ),
                     ),
-                    const Spacer(),
+                    const SizedBox(height: 20),
                     Expanded(
-                      flex: 3, // takes 30% of available width
-                      child:       SfBarcodeGenerator(
+                      flex: 8, // takes 30% of available width
+                      child: SfBarcodeGenerator(
                         value: "${txtController.text}.3",
                         showValue: true,
+                        textStyle: const TextStyle(fontWeight: FontWeight.bold),
                       ),
                     ),
-                    const Spacer(),
+                    const SizedBox(height: 20),
                     Expanded(
-                      flex: 3, // takes 30% of available width
-                      child:       SfBarcodeGenerator(
+                      flex: 8, // takes 30% of available width
+                      child: SfBarcodeGenerator(
                         value: "${txtController.text}.4",
                         showValue: true,
+                        textStyle: const TextStyle(fontWeight: FontWeight.bold),
                       ),
                     ),
-                    const Spacer(),
-                    Expanded(
-                        flex: 3, // takes 30% of available width
-                        child: Image(image: AssetImage("assets/logo.png"))
-                    ),
-                  ]
-                ),
-              )
-            ),
+                    const SizedBox(height: 20),
+                    const Expanded(
+                        flex: 8, // takes 30% of available width
+                        child: Image(image: AssetImage("assets/logo.png"))),
+                  ]),
+                )),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        tooltip: 'Increment',
+        tooltip: 'Imprimir',
         onPressed: () {
-          print("ok");
           renderImage();
         },
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: const Icon(Icons.print),
+      ),
     );
   }
 
-  Future<String?> generateBarcode() async {
-    print(txtController.text);
-//     // Create a DataMatrix barcode
-//     final dm = Barcode.dataMatrix();
-//
-// // Generate a SVG with "Hello World!"
-//     final svg = dm.toSvg('Hello World!', width: 200, height: 200);
-//
-//     final svgImage = pw.SvgImage(svg: svg);
-//
-//     pdf.addPage(pw.Page(build: (pw.Context context) {
-//       return pw.Center(
-//         child: svgImage,
-//       ); // Center
-//     })); // Page
-//
-// // Save the image
-// //     await File('barcode.svg').writeAsString(svg);
-//   print(svg);
-    return null;
-  }
   void renderImage() async {
-    final boundary = globalKey.currentContext?.findRenderObject() as RenderRepaintBoundary?;
+    final boundary =
+        globalKey.currentContext?.findRenderObject() as RenderRepaintBoundary?;
     final image = await boundary?.toImage();
     final byteData = await image?.toByteData(format: ImageByteFormat.png);
     final imageBytes = byteData?.buffer.asUint8List();
     if (imageBytes != null) {
       final directory = await getApplicationDocumentsDirectory();
-      final imagePath = await File('${directory.path}/container_image.png').create();
-      print(directory);
-      print(imagePath);
+      final imagePath =
+          await File('${directory.path}\\container_image.png').create();
+
       await imagePath.writeAsBytes(imageBytes);
+
+      final img = pw.MemoryImage(imageBytes);
+
+      final doc = pw.Document();
+      doc.addPage(pw.Page(
+          pageFormat: PdfPageFormat.a4,
+          build: (pw.Context context) {
+            return pw.Center(
+              child: pw.Image(img),
+            ); // Center
+          })); // Page
+      await Printing.layoutPdf(
+          onLayout: (PdfPageFormat format) async => doc.save());
     }
   }
-
 }
-
-
