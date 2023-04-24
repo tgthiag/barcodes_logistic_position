17a6a5a15897f46276ed7ff697b6d64c69bdac52
tgthiag <tgthiag@gmail.com>
2023-04-24 15:37:44 -0300

    Code format

--- lib/barcodes_paper_a4.dart
+++ lib/barcodes_paper_a4.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:syncfusion_flutter_barcodes/barcodes.dart';
 
-class BarcodesPapera4 extends StatelessWidget{
+class BarcodesPapera4 extends StatelessWidget {
   TextEditingController? txtController;
   final int intHolder;
   String? stringHolder;
 
-  BarcodesPapera4({super.key, required this.txtController, required this.intHolder, required this.stringHolder});
+  BarcodesPapera4(
+      {super.key,
+      required this.txtController,
+      required this.intHolder,
+      required this.stringHolder});
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +21,6 @@ class BarcodesPapera4 extends StatelessWidget{
           Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-
                 Container(
                   margin: const EdgeInsets.only(bottom: 30),
                   width: 180,
@@ -25,44 +28,37 @@ class BarcodesPapera4 extends StatelessWidget{
                   child: SfBarcodeGenerator(
                     value: "${txtController?.text}.5",
                     showValue: true,
-                    textStyle:
-                    const TextStyle(fontWeight: FontWeight.bold),
+                    textStyle: const TextStyle(fontWeight: FontWeight.bold),
                   ),
                 ),
                 Container(
-                  margin: const EdgeInsets.symmetric(
-                      vertical: 30),
+                  margin: const EdgeInsets.symmetric(vertical: 30),
                   width: 180,
                   height: 58,
                   child: SfBarcodeGenerator(
                     value: "${txtController?.text}.4",
                     showValue: true,
-                    textStyle:
-                    const TextStyle(fontWeight: FontWeight.bold),
+                    textStyle: const TextStyle(fontWeight: FontWeight.bold),
                   ),
                 ),
                 Container(
-                  margin: const EdgeInsets.symmetric(
-                      vertical: 30),
+                  margin: const EdgeInsets.symmetric(vertical: 30),
                   width: 180,
                   height: 58,
                   child: SfBarcodeGenerator(
                     value: "${txtController?.text}.3",
                     showValue: true,
-                    textStyle:
-                    const TextStyle(fontWeight: FontWeight.bold),
+                    textStyle: const TextStyle(fontWeight: FontWeight.bold),
                   ),
                 ),
                 Container(
-                  margin: const EdgeInsets.symmetric(
-                      vertical: 30),
+                  margin: const EdgeInsets.symmetric(vertical: 30),
                   width: 180,
                   height: 58,
                   child: SfBarcodeGenerator(
                     value: "${txtController?.text}.2",
                     showValue: true,
-                    textStyle:
-                    const TextStyle(fontWeight: FontWeight.bold),
+                    textStyle: const TextStyle(fontWeight: FontWeight.bold),
                   ),
                 ),
                 Container(
@@ -72,16 +68,14 @@ class BarcodesPapera4 extends StatelessWidget{
                   child: SfBarcodeGenerator(
                     value: "${txtController?.text}.1",
                     showValue: true,
-                    textStyle:
-                    const TextStyle(fontWeight: FontWeight.bold),
+                    textStyle: const TextStyle(fontWeight: FontWeight.bold),
                   ),
                 ),
                 Container(
                     width: 180,
                     height: 60,
                     margin: const EdgeInsets.only(top: 10),
-                    child: const Image(
-                        image: AssetImage("assets/logo.png"))),
+                    child: const Image(image: AssetImage("assets/logo.png"))),
               ]),
           Column(children: <Widget>[
             Container(
@@ -89,13 +83,13 @@ class BarcodesPapera4 extends StatelessWidget{
               width: 180,
               height: 58,
               child: SfBarcodeGenerator(
-                value: (txtController!.text.length >= 1 &&
-                    txtController?.text[1] == "0" && txtController?.text[2] != "9")
+                value: (txtController!.text.isNotEmpty &&
+                        txtController?.text[1] == "0" &&
+                        txtController?.text[2] != "9")
                     ? "${"${stringHolder}0$intHolder"}.5"
                     : "${stringHolder! + intHolder.toString()}.5",
                 showValue: true,
-                textStyle:
-                const TextStyle(fontWeight: FontWeight.bold),
+                textStyle: const TextStyle(fontWeight: FontWeight.bold),
               ),
             ),
             Container(
@@ -103,13 +97,13 @@ class BarcodesPapera4 extends StatelessWidget{
               width: 180,
               height: 58,
               child: SfBarcodeGenerator(
-                value: (txtController!.text.length >= 1 &&
-                    txtController?.text[1] == "0" && txtController?.text[2] != "9")
+                value: (txtController!.text.isNotEmpty &&
+                        txtController?.text[1] == "0" &&
+                        txtController?.text[2] != "9")
                     ? "${"${stringHolder}0$intHolder"}.4"
                     : "${stringHolder! + intHolder.toString()}.4",
                 showValue: true,
-                textStyle:
-                const TextStyle(fontWeight: FontWeight.bold),
+                textStyle: const TextStyle(fontWeight: FontWeight.bold),
               ),
             ),
             Container(
@@ -117,13 +111,13 @@ class BarcodesPapera4 extends StatelessWidget{
               width: 180,
               height: 58,
               child: SfBarcodeGenerator(
-                value: (txtController!.text.length >= 1 &&
-                    txtController?.text[1] == "0" && txtController?.text[2] != "9")
+                value: (txtController!.text.isNotEmpty &&
+                        txtController?.text[1] == "0" &&
+                        txtController?.text[2] != "9")
                     ? "${"${stringHolder}0$intHolder"}.3"
                     : "${stringHolder! + intHolder.toString()}.3",
                 showValue: true,
-                textStyle:
-                const TextStyle(fontWeight: FontWeight.bold),
+                textStyle: const TextStyle(fontWeight: FontWeight.bold),
               ),
             ),
             Container(
@@ -131,13 +125,13 @@ class BarcodesPapera4 extends StatelessWidget{
               width: 180,
               height: 58,
               child: SfBarcodeGenerator(
-                value: (txtController!.text.length >= 1 &&
-                    txtController?.text[1] == "0" && txtController?.text[2] != "9")
+                value: (txtController!.text.isNotEmpty &&
+                        txtController?.text[1] == "0" &&
+                        txtController?.text[2] != "9")
                     ? "${"${stringHolder}0$intHolder"}.2"
                     : "${stringHolder! + intHolder.toString()}.2",
                 showValue: true,
-                textStyle:
-                const TextStyle(fontWeight: FontWeight.bold),
+                textStyle: const TextStyle(fontWeight: FontWeight.bold),
               ),
             ),
             Container(
@@ -145,25 +139,23 @@ class BarcodesPapera4 extends StatelessWidget{
               width: 180,
               height: 58,
               child: SfBarcodeGenerator(
-                value: (txtController!.text.length >= 1 &&
-                    txtController?.text[1] == "0" && txtController?.text[2] != "9")
+                value: (txtController!.text.isNotEmpty &&
+                        txtController?.text[1] == "0" &&
+                        txtController?.text[2] != "9")
                     ? "${"${stringHolder}0$intHolder"}.1"
                     : "${stringHolder! + intHolder.toString()}.1",
                 showValue: true,
-                textStyle:
-                const TextStyle(fontWeight: FontWeight.bold),
+                textStyle: const TextStyle(fontWeight: FontWeight.bold),
               ),
             ),
             Container(
                 width: 180,
                 height: 60,
                 margin: const EdgeInsets.only(top: 10),
-                child: const Image(
-                    image: AssetImage("assets/logo.png"))),
+                child: const Image(image: AssetImage("assets/logo.png"))),
           ]),
         ],
       ),
     );
   }
-
 }
